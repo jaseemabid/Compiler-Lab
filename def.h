0b8ed96d3a0125d3fc00ef59a3ec4a9b1aba6293
Jaseem Abid <jaseemabid@gmail.com>
2011-09-25 16:08:08 +0530

    Node changed -> node posted on website

--- def.h
+++ def.h
@@ -1,13 +1,25 @@
 /* Common header file */
 
+/** Sample Expression Tree Node Structure **/
+
 struct tNode {
-	int value;
-	char op;
-	struct tNode* left;
-	struct tNode* right;
-};
+	int TYPE;
+					/* Integer, Boolean or Void (for statements) */
+					/* Must point to the type expression tree for user defined types */
+	int NODETYPE;
+					/* this field should carry following information:
+					* a) operator : (+,*,/ etc.) for expressions
+					* b) statement Type : (WHILE, READ etc.) for statements */
+	char* NAME;
+					/* For Identifiers/Functions */
+	int VALUE;
+					/* for constants */
+	tNode *ArgList;
+					/* List of arguments for functions */
+	tNode *left, *right;
+}
 
 struct tNode* root;
 struct tNode* temp;
 
-
+/* struct tNode *TreeCreate(TYPE,NODETYPE,VALUE,NAME,left,right); */
